da37e204cef47ebe5f57b0c486ed7c43d5001fde
grolox69 <karl.gharios@gmail.com>
2022-01-31 20:21:39 +0200

    Remove category details Text and Image

---
+++ src/component/CategoryDetails/CategoryDetails.component.js
@@ -0,0 +1,14 @@
+import { CategoryDetails as SourceCategoryDetails } from 'SourceComponent/CategoryDetails/CategoryDetails.component';
+
+/** @namespace ScandiTest/Component/CategoryDetails/Component */
+export class CategoryDetailsComponent extends SourceCategoryDetails {
+    renderCategoryDescription() {
+        return null;
+    }
+
+    renderCategoryImage() {
+        return null;
+    }
+}
+
+export default CategoryDetailsComponent;
